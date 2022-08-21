64ac58542b1c785d78aca04a387e67771b4d2741
ehmicky <ehmicky@gmail.com>
2022-08-21 21:27:43 +0200

    Improve types

--- src/main.d.ts
+++ src/main.d.ts
@@ -18,6 +18,60 @@ export interface Options {
   readonly lowPriority?: boolean
 }
 
+type DeepMergeObjects<
+  One extends object,
+  Two extends object,
+  LowPriority extends boolean,
+> = {
+  [oneKey in keyof One]: oneKey extends keyof Two
+    ? DeepMerge<One[oneKey], Two[oneKey], LowPriority>
+    : One[oneKey]
+} & {
+  [twoKey in Exclude<keyof Two, keyof One>]: Two[twoKey]
+}
+
+type DeepMergeObjectsOrArrays<
+  One extends object,
+  Two extends object,
+  LowPriority extends boolean,
+> = One extends any[]
+  ? Two extends any[]
+    ? [...One, ...Two]
+    : DeepMergeObjects<One, Two, LowPriority>
+  : DeepMergeObjects<One, Two, LowPriority>
+
+type DeepMerge<One, Two, LowPriority extends boolean> = Two extends object
+  ? One extends object
+    ? DeepMergeObjectsOrArrays<One, Two, LowPriority>
+    : Two
+  : Two extends undefined
+  ? LowPriority extends true
+    ? One
+    : Two
+  : Two
+
+type DeepMergeWithPriority<
+  Props,
+  ErrorArg,
+  LowPriority extends boolean | undefined,
+> = LowPriority extends true
+  ? DeepMerge<Props, ErrorArg, true>
+  : DeepMerge<ErrorArg, Props, false>
+
+type CoreErrorProps =
+  | 'name'
+  | 'message'
+  | 'stack'
+  | 'cause'
+  | 'error'
+  | 'constructor'
+  | 'toString'
+  | 'hasOwnProperty'
+  | 'isPrototypeOf'
+  | 'propertyIsEnumerable'
+  | 'valueOf'
+  | 'toLocaleString'
+
 /**
  * Assign `props` to `error`.
  *
@@ -29,8 +83,21 @@ export interface Options {
  * console.log(error.prop) // { one: [true, false], two: true }
  * ```
  */
-export default function setErrorProps(
-  error: Error,
-  props: Error | {},
-  options?: Options,
-): void
+export default function setErrorProps<
+  ErrorArg extends Error,
+  Props extends object,
+  OptionsArg extends Options,
+>(
+  error: ErrorArg,
+  props: Props,
+  options?: OptionsArg,
+): Error &
+  Pick<ErrorArg, CoreErrorProps & keyof ErrorArg> &
+  Omit<
+    DeepMergeWithPriority<
+      Props,
+      ErrorArg,
+      OptionsArg['lowPriority'] extends true ? true : false
+    >,
+    CoreErrorProps
+  >
--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -8,7 +8,7 @@ import {
 import setErrorProps, { Options } from './main.js'
 
 const error = new Error('test')
-expectType<void>(setErrorProps(error, {}))
+expectAssignable<Error>(setErrorProps(error, {}))
 
 setErrorProps(error, error)
 setErrorProps(error, {})
@@ -20,3 +20,139 @@ setErrorProps(error, {}, { lowPriority: true })
 expectAssignable<Options>({ lowPriority: true })
 expectError(setErrorProps(error, {}, { lowPriority: 'true' }))
 expectNotAssignable<Options>({ lowPriority: 'true' })
+
+expectType<1>(setErrorProps({} as Error & { a: 0 }, { a: 1 as const }).a)
+expectType<0>(
+  setErrorProps(
+    {} as Error & { a: 0 },
+    { a: 1 as const },
+    { lowPriority: true },
+  ).a,
+)
+expectType<1>(setErrorProps({} as Error & { a: {} }, { a: 1 as const }).a)
+expectType<{}>(
+  setErrorProps(
+    {} as Error & { a: {} },
+    { a: 1 as const },
+    { lowPriority: true },
+  ).a,
+)
+expectType<{}>(setErrorProps({} as Error & { a: 0 }, { a: {} as const }).a)
+expectType<0>(
+  setErrorProps(
+    {} as Error & { a: 0 },
+    { a: {} as const },
+    { lowPriority: true },
+  ).a,
+)
+expectType<1>(
+  setErrorProps({} as Error & { a: { b: 0 } }, { a: { b: 1 } as const }).a.b,
+)
+expectType<0>(
+  setErrorProps(
+    {} as Error & { a: { b: 0 } },
+    { a: { b: 1 } as const },
+    { lowPriority: true },
+  ).a.b,
+)
+expectType<0>(
+  setErrorProps({} as Error & { a: { b: 0 } }, { a: { c: 1 } as const }).a.b,
+)
+expectType<0>(
+  setErrorProps(
+    {} as Error & { a: { b: 0 } },
+    { a: { c: 1 } as const },
+    { lowPriority: true },
+  ).a.b,
+)
+expectType<1>(
+  setErrorProps({} as Error & { a: { c: 0 } }, { a: { b: 1 } as const }).a.b,
+)
+expectType<1>(
+  setErrorProps(
+    {} as Error & { a: { c: 0 } },
+    { a: { b: 1 } as const },
+    { lowPriority: true },
+  ).a.b,
+)
+expectType<1>(setErrorProps({} as Error & { a: [0] }, { a: 1 as const }).a)
+expectType<[0]>(
+  setErrorProps(
+    {} as Error & { a: [0] },
+    { a: 1 as const },
+    { lowPriority: true },
+  ).a,
+)
+expectType<[1]>(setErrorProps({} as Error & { a: 0 }, { a: [1] as [1] }).a)
+expectType<0>(
+  setErrorProps(
+    {} as Error & { a: 0 },
+    { a: [1] as [1] },
+    { lowPriority: true },
+  ).a,
+)
+expectType<[0, 1]>(setErrorProps({} as Error & { a: [0] }, { a: [1] as [1] }).a)
+expectType<[1, 0]>(
+  setErrorProps(
+    {} as Error & { a: [0] },
+    { a: [1] as [1] },
+    { lowPriority: true },
+  ).a,
+)
+expectType<1>(
+  setErrorProps({} as Error & { a: undefined }, { a: 1 as const }).a,
+)
+expectType<1>(
+  setErrorProps(
+    {} as Error & { a: undefined },
+    { a: 1 as const },
+    { lowPriority: true },
+  ).a,
+)
+expectType<1>(setErrorProps({} as Error, { a: 1 as const }).a)
+expectType<1>(
+  setErrorProps({} as Error, { a: 1 as const }, { lowPriority: true }).a,
+)
+expectType<undefined>(setErrorProps({} as Error & { a: 1 }, { a: undefined }).a)
+expectType<1>(
+  setErrorProps({} as Error & { a: 1 }, { a: undefined }, { lowPriority: true })
+    .a,
+)
+expectType<1>(setErrorProps({} as Error & { a: 1 }, {}).a)
+expectType<1>(
+  setErrorProps({} as Error & { a: 1 }, {}, { lowPriority: true }).a,
+)
+const symbol = Symbol('test')
+expectType<1>(
+  setErrorProps({} as Error & { [symbol]: 0 }, { [symbol]: 1 as const })[
+    symbol
+  ],
+)
+expectType<0>(
+  setErrorProps(
+    {} as Error & { [symbol]: 0 },
+    { [symbol]: 1 as const },
+    { lowPriority: true },
+  )[symbol],
+)
+expectType<'0'>(
+  setErrorProps({} as Error & { name: '0' }, { name: '1' as const }).name,
+)
+expectType<'0'>(
+  setErrorProps(
+    {} as Error & { name: '0' },
+    { name: '1' as const },
+    { lowPriority: true },
+  ).name,
+)
+expectAssignable<() => '0'>(
+  setErrorProps({} as Error & { toString: () => '0' }, { toString: () => '1' })
+    .toString,
+)
+expectAssignable<() => '0'>(
+  setErrorProps(
+    {} as Error & { toString: () => '0' },
+    { toString: () => '1' },
+    { lowPriority: true },
+  ).toString,
+)
