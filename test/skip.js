3710f5e5d568741088ee6c58ab55c0f7c0ab0339
ehmicky <ehmicky@gmail.com>
2022-08-21 19:34:15 +0200

    Add more tests

--- test/skip.js
+++ test/skip.js
@@ -46,3 +46,45 @@ test('Non-enumerable properties are ignored deeply when merged', (t) => {
 test('Non-enumerable properties are included deeply when not merged', (t) => {
   t.true('prop' in setProps({}, { deep: nonEnumerableProps }).deep)
 })
+
+test('Handle non-writable properties', (t) => {
+  // eslint-disable-next-line fp/no-mutating-methods
+  const nonWritableObject = Object.defineProperty({}, 'prop', {
+    value: true,
+    enumerable: true,
+    writable: false,
+    configurable: true,
+  })
+  t.true(setProps(nonWritableObject, { prop: false }).prop)
+})
+
+const getProxyObject = function () {
+  return {
+    changed: false,
+    // eslint-disable-next-line fp/no-get-set
+    get prop() {
+      return 'prop'
+    },
+    // eslint-disable-next-line fp/no-get-set
+    set prop(value) {
+      // eslint-disable-next-line fp/no-mutation, fp/no-this
+      this.changed = value
+    },
+  }
+}
+
+test('Sets value is different', (t) => {
+  t.true(setProps(getProxyObject(), { prop: true }).changed)
+})
+
+test('Does not set value is identical', (t) => {
+  t.false(setProps(getProxyObject(), { prop: 'prop' }).changed)
+})
+
+test('Sets undefined if high priority', (t) => {
+  t.true('prop' in setProps({}, { prop: undefined }))
+})
+
+test('Does not set undefined if low priority', (t) => {
+  t.false('prop' in setProps({}, { prop: undefined }, { lowPriority: true }))
+})
