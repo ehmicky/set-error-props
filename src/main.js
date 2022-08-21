b64a22bab02f4fcbfd93806390ebea3e0bca136d
ehmicky <ehmicky@gmail.com>
2022-08-21 17:39:50 +0200

    Initial implementation

--- src/main.js
+++ src/main.js
@@ -1,3 +1,32 @@
-export default function setErrorProps(value) {
-  return value
+// Merge error properties, shallowly, with parent error having priority
+// Do not merge inherited properties nor non-enumerable properties.
+// Works with symbol properties.
+export default function setErrorProps(error, props) {
+  // eslint-disable-next-line fp/no-loops
+  for (const propName of Reflect.ownKeys(props)) {
+    mergeProp(error, props, propName)
+  }
 }
+
+const mergeProp = function (error, props, propName) {
+  if (propName in error) {
+    return
+  }
+
+  const descriptor = Object.getOwnPropertyDescriptor(props, propName)
+
+  if (descriptor !== undefined && !CORE_ERROR_PROPS.has(propName)) {
+    // eslint-disable-next-line fp/no-mutating-methods
+    Object.defineProperty(error, propName, descriptor)
+  }
+}
+
+// Do not copy core error properties.
+// Does not assume they are not enumerable.
+const CORE_ERROR_PROPS = new Set([
+  'name',
+  'message',
+  'stack',
+  'cause',
+  'errors',
+])
