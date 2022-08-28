ed7267f3128b580506ac141fcb7298847e9eb161
ehmicky <ehmicky@gmail.com>
2022-08-28 21:34:37 +0200

    Refactor prototype pollution logic

--- src/skip.js
+++ src/skip.js
@@ -1,19 +1,20 @@
 // Do not override core error properties
 export const shouldSkipProp = function (props, propName) {
-  return CORE_ERROR_PROPS.has(propName) || !isEnum.call(props, propName)
+  return (
+    propName in CHECK_ERROR ||
+    IGNORED_PROPS.has(propName) ||
+    !isEnum.call(props, propName)
+  )
 }
 
-const CORE_ERROR_PROPS = new Set([
-  'name',
-  'message',
-  'stack',
-  'cause',
-  'errors',
-  ...Reflect.ownKeys(Error.prototype),
-  ...Reflect.ownKeys(Object.prototype),
-])
-
 const { propertyIsEnumerable: isEnum } = Object.prototype
+
+// Uses `key in error` to handle any current and future error|object properties
+const CHECK_ERROR = new Error('check')
+// Those properties are either optional, set dynamically by `Error` constructor
+// (as opposed to being on the `prototype`) or have special meaning like
+// `prototype`
+const IGNORED_PROPS = new Set(['prototype', 'errors', 'cause'])
 
 // `undefined` values are set, since `propName in error` might be `false`, but
 // only if `lowPriority` is `false`.
