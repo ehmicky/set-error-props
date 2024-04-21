96c91ffd0fc6d671a0c50dd2cee9812fffedb000
ehmicky <ehmicky@gmail.com>
2024-04-21 23:41:54 +0100

    Fix linting

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -65,12 +65,10 @@ expectType<'0'>(
     .name,
 )
 expectAssignable<() => '0'>(
-  // eslint-disable-next-line @typescript-eslint/unbound-method
   setErrorProps({} as Error & { toString: () => '0' }, { toString: () => '1' })
     .toString,
 )
 expectAssignable<() => '0'>(
-  // eslint-disable-next-line @typescript-eslint/unbound-method
   setErrorProps(
     {} as Error & { toString: () => '0' },
     { toString: () => '1' },
